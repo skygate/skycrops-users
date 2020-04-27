92480808e802fed7030ff6932537fd1a91c1bdca
wojciechczarnecki <wczarnecky@gmail.com>
2020-04-27 15:25:59 +0200

    [SKYC-153] Add migrations

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from config import Config
 application = Flask(__name__)
 application.config.from_object(Config)
 db = SQLAlchemy(application)
-migrate = Migrate(application)
+migrate = Migrate(application, db)
 
 from .routes import api
 
