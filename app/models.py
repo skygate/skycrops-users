3cd1a6ae818e6faa56302cd4f0ad5203d92ea39a
wojciechczarnecki <wczarnecky@gmail.com>
2020-04-27 16:52:35 +0200

    [SKYC-153] Add orchards to the database

--- app/models.py
+++ app/models.py
@@ -5,6 +5,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
+    orchards = db.relationship('Orchard', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f"<ID: {self.id} User: {self.username}>"
+
+
+class Orchard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rows = db.Column(db.Integer)
+    trees = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"<Orchard ID: {self.id} Rows: {self.rows}, Trees: {self.trees}>"
