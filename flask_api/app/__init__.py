c9b929a818c6494b8340bf9bd2e8d924c27be1b5
wojciechczarnecki <wczarnecky@gmail.com>
2020-04-29 00:29:57 +0200

    [SKYC-153] Add cors

--- flask_api/app/__init__.py
+++ flask_api/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 application = Flask(__name__)
+CORS(application)
 application.secret_key = "super secret key"
 application.config.from_object(Config)
 db = SQLAlchemy(application)
