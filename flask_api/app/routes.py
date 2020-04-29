33c82b687dc5664b1b2080e48dc061d265cd81da
wojciechczarnecki <wczarnecky@gmail.com>
2020-04-29 13:45:30 +0200

    [SKYC-153] Fix jsonify

--- flask_api/app/routes.py
+++ flask_api/app/routes.py
@@ -22,25 +22,23 @@ api = Blueprint("api", __name__)
 
 @api.route("/healthchecker", methods=["GET"])
 def healthchecker():
-    return jsonify({"status": "I am alive!"}), 200
+    return jsonify(status="I am alive!"), 200
 
 
 @api.route("/users", methods=["POST"])
 def create_user():
     name = request.form.get(USERNAME_KEY)
     if not name:
-        return jsonify({"status": "Name can't be empty."}), 400
+        return jsonify(status="Name can't be empty."), 400
 
     password = request.form.get(PASSWORD_KEY)
     if not password:
-        return jsonify({"status": "Password can't be empty."}), 400
+        return jsonify(status="Password can't be empty."), 400
 
     is_name_duplicated = User.query.filter_by(username=name).first()
     if is_name_duplicated:
         return (
-            jsonify(
-                {"status": "This name is already used, please use a different one."}
-            ),
+            jsonify(status="This name is already used, please use a different one."),
             400,
         )
 
@@ -49,14 +47,14 @@ def create_user():
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({"status": f"{name} saved to the database."})
+    return jsonify(status=f"{name} saved to the database.")
 
 
 @api.route("/users/database", methods=["POST"])
 def show_database():
     users = User.query.all()
     orchards = Orchard.query.all()
-    return jsonify({"users": str(users), "orchards": str(orchards)})
+    return jsonify(users=str(users), orchards=str(orchards))
 
 
 @api.route("/users/database/clear", methods=["POST"])
@@ -64,24 +62,24 @@ def clear_database():
     db.drop_all()
     db.create_all()
     db.session.commit()
-    return jsonify({"status": "Database clear!"})
+    return jsonify(status="Database clear!")
 
 
 @api.route("/users/orchards", methods=["POST"])
 def create_orchard():
     if not current_user.is_authenticated:
-        return jsonify({"status": "You have to login first!"}), 400
+        return jsonify(status="You have to login first!"), 400
 
     coords = request.files.get(COORDS_KEY)
     distance_between_rows = request.form.get(ROWS_KEY)
     distance_between_trees = request.form.get(TREES_KEY)
 
     if not coords:
-        return jsonify({"status": "Failed! Coordinates can't be empty!"}), 400
+        return jsonify(status="Failed! Coordinates can't be empty!"), 400
 
     if not coords.filename.endswith(".csv"):
         return (
-            jsonify({"status": "Failed! Coordinates file must have a .csv extension!"}),
+            jsonify(status="Failed! Coordinates file must have a .csv extension!"),
             400,
         )
 
@@ -92,9 +90,7 @@ def create_orchard():
             distance_between_rows = float(distance_between_rows)
         except ValueError:
             return (
-                jsonify(
-                    {"status": "Failed! Distance between rows must be a float number!"}
-                ),
+                jsonify(status="Failed! Distance between rows must be a float number!"),
                 400,
             )
 
@@ -106,7 +102,7 @@ def create_orchard():
         except ValueError:
             return (
                 jsonify(
-                    {"status": "Failed! Distance between trees must be a float number!"}
+                    status="Failed! Distance between trees must be a float number!"
                 ),
                 400,
             )
@@ -125,48 +121,48 @@ def create_orchard():
     db.session.add(orchard)
     db.session.commit()
 
-    return jsonify({"status": f"{coords.filename} saved to the database."})
+    return jsonify(status=f"{coords.filename} saved to the database.")
 
 
 @api.route("/login", methods=["POST"])
 def login():
     if current_user.is_authenticated:
-        return jsonify({"status": "Failed! Someone is already logged in!"}), 400
+        return jsonify(status="Failed! Someone is already logged in!"), 400
 
     name = request.form.get(USERNAME_KEY)
     if not name:
-        return jsonify({"status": "Name can't be empty."}), 400
+        return jsonify(status="Name can't be empty."), 400
 
     password = request.form.get(PASSWORD_KEY)
     if not password:
-        return jsonify({"status": "Password can't be empty."}), 400
+        return jsonify(status="Password can't be empty."), 400
 
     user = User.query.filter_by(username=name).first()
     if not user or not user.check_password(password):
-        return jsonify({"status": "Invalid username or password"}), 400
+        return jsonify(status="Invalid username or password"), 400
 
     login_user(user)
-    return jsonify({"status": f"{name} logged in successfully!"})
+    return jsonify(status=f"{name} logged in successfully!")
 
 
 @api.route("/logout", methods=["POST"])
 def logout():
     logout_user()
-    return jsonify({"status": "Successfully logged out!"})
+    return jsonify(status="Successfully logged out!")
 
 
 @api.route("/orchards", methods=["POST"])
 def get_orchards_data():
     if not current_user.is_authenticated:
-        return jsonify({"status": "You have to login first!"}), 400
+        return jsonify(status="You have to login first!"), 400
     orchards = current_user.orchards.all()
     orchards_data = {
         orchard.id: {"rows": orchard.rows, "treesInEachRow": orchard.trees}
         for orchard in orchards
     }
-    return jsonify({"orchards": orchards_data})
+    return jsonify(orchards=orchards_data)
 
 
 @api.route("/is_logged", methods=["POST"])
 def is_logged():
-    return jsonify({"status": current_user.is_authenticated})
+    return jsonify(status=current_user.is_authenticated)
